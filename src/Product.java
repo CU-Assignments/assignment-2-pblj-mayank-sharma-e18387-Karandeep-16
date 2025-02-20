cf11be4c1784d8b9026fd093b34230f030c71cb7
Karandeep-16 <Karandeep3116@gmail.com>
2025-02-20 23:13:02 +0530

    Add files via upload

---
+++ src/Product.java
@@ -0,0 +1,24 @@
+class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void displayProductDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Product product1 = new Product(101, "Laptop", 75000);
+
+        product1.displayProductDetails();
+    }
+}
